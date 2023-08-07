e8514f236c46583b46736a5ea2d5d4e81d8141a1
Ankur <ankurchanda198@gmail.com>
2023-08-08 00:39:26 +0530

    Added assignment 6

---
+++ q6.java
@@ -0,0 +1,19 @@
+interface printable{
+    void print();
+}
+interface showable{
+    void show();
+}
+class A implements printable,showable{
+    public void print(){
+        System.out.println("Hello");
+    }
+    public void show(){
+        System.out.println("AIML");
+    }
+    public static void main(String args[]){
+        A obj= new A();
+        obj.print();
+        obj.show();
+    }
+}
